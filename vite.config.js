c161d686cc40182699cde5d99c86ea5635c48979
Srii Rohit Prakash <sprakash1@binghamton.edu>
2025-09-01 17:34:37 -0400

    Added new features

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio_threejs/', // Your repository name from the screenshot
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
